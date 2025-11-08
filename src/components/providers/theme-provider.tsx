8fced27f7d911ed2c40f7dc13d7239b707b0e796
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-11-08 18:21:34 +0530

    feat: add ThemeProvider component for theme management

---
+++ src/components/providers/theme-provider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+interface ThemeProviderProps {
+  children: React.ReactNode;
+  [key: string]: any;
+}
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  );
+}
