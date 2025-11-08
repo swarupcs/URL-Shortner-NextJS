5fe7309bb08fd4a3f7d084bc76bfc0a3fa03cd43
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-11-08 18:21:08 +0530

    feat: add getUrlByShortCode function to retrieve original URL by short code

---
+++ src/server/actions/urls/get-url.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import { ApiResponse } from "@/lib/types";
+import { db, eq } from "@/server/db";
+import { urls } from "@/server/db/schema";
+
+export async function getUrlByShortCode(shortCode: string): Promise<
+  ApiResponse<{
+    originalUrl: string;
+    flagged?: boolean;
+    flagReason?: string | null;
+  }>
+> {
+  try {
+    const url = await db.query.urls.findFirst({
+      where: (urls, { eq }) => eq(urls.shortCode, shortCode),
+    });
+
+    if (!url) {
+      return {
+        success: false,
+        error: "URL not found",
+      };
+    }
+
+    await db
+      .update(urls)
+      .set({
+        clicks: url.clicks + 1,
+        updatedAt: new Date(),
+      })
+      .where(eq(urls.shortCode, shortCode));
+
+    return {
+      success: true,
+      data: {
+        originalUrl: url.originalUrl,
+        flagged: url.flagged || false,
+        flagReason: url.flagReason || null,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      error: "An error occurred while fetching the URL",
+    };
+  }
+}
