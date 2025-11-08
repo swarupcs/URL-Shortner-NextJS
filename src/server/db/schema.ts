a9b58edc8abad5f61e17cc9bb43298731b869126
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-11-08 15:40:57 +0530

    add user schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,10 +1,18 @@
 import {
   integer,
+  pgEnum,
   pgTable,
   serial,
   timestamp,
   varchar,
+  text,
+  primaryKey,
+  boolean,
 } from 'drizzle-orm/pg-core';
+
+// Define user roles enum
+export const userRoleEnum = pgEnum("user_role", ["user", "admin"]);
+
 
 export const urls = pgTable('urls', {
   id: serial('id').primaryKey(),
@@ -13,4 +21,17 @@ export const urls = pgTable('urls', {
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
   clicks: integer('clicks').default(0).notNull(),
+});
+
+// Define user table
+export const users = pgTable('users', {
+  id: varchar('id', { length: 255 }).notNull().primaryKey(),
+  name: varchar('name', { length: 255 }),
+  email: varchar('email', { length: 255 }).notNull().unique(),
+  emailVerified: timestamp('emailVerified', { mode: 'date' }),
+  image: text('image'),
+  password: text('password'),
+  role: userRoleEnum('role').default('user').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
 });
